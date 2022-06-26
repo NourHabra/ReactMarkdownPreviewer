7603fbdc640b568df1c2f87dd0b1b73ea642fc3b
Nour Habra <NourHBSU@gmail.com>
2022-06-26 12:25:17 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { marked } from "marked";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+marked.use({
+	breaks: true,
+});
+
+const initMarkup = `
+# Hello World!
+## I am Nour Habra and this is my React Markdown Previewer.
+
+[Cute Cat](https://www.youtube.com/watch?v=dQw4w9WgXcQ) 
+
+\`const veryCoolProject = true;\`
+
+I also created this autopilot for cars:
+\`\`\`
+if (aboutToCrash()){
+	dont();
+}
+\`\`\`
+### Things I'd never do:
+- give you up
+> never gonna give you up.
+> -Rick Astley
+
+![Cute Cat](https://i.pinimg.com/564x/c5/8a/d4/c58ad46c9e1a1d3272ab6eb4b55dc06f.jpg)
+**Thanks for visiting**
+`;
+
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			markup: initMarkup,
+		};
+		this.handleChange = this.handleChange.bind(this);
+	}
+
+	handleChange(event) {
+		this.setState((state) => ({ markup: event.target.value }));
+	}
+
+	render() {
+		return (
+			<div className="App">
+				{/* <div className="header">
+					<p>pre-markdown</p>
+				</div> */}
+				<div className="editors">
+					<div className="left">
+						<div className="iconbar">
+							<p>Editor</p>
+						</div>
+						<textarea
+							type="textbox"
+							onChange={this.handleChange}
+							className="premarkdown"
+							id="editor"
+						>
+							{this.state.markup}
+						</textarea>
+					</div>
+					<div
+						className="right"
+						id="preview"
+						dangerouslySetInnerHTML={{
+							__html: marked.parse(this.state.markup),
+						}}
+					></div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
